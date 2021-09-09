cbdb940ed27b831cfd0b9a3d037c2b034ec79950
Tiago Santos <tiago881@gmail.com>
2021-09-09 09:49:58 -0300

    Operadores de comparação

---
+++ operadores-logicos/operadores-de-comparacao.js
@@ -0,0 +1,8 @@
+// igual (==) Retorna verdadeiro caso os operandos sejam iguais.
+// Não igual (!=)	Retorna verdadeiro caso os operandos não sejam iguais.	
+// Estritamente igual (===)	Retorna verdadeiro caso os operandos sejam iguais e do mesmo tipo. 
+// Estritamente não igual (!==)	Retorna verdadeiro caso os operandos não sejam iguais e/ou não sejam do mesmo tipo.	
+// Maior que (>)	Retorna verdadeiro caso o operando da esquerda seja maior que o da direita.	
+// Maior que ou igual (>=)	Retorna verdadeiro caso o operando da esquerda seja maior ou igual ao da direita.	
+// Menor que (<)	Retorna verdadeiro caso o operando da esquerda seja menor que o da direita.	
+// Menor que ou igual (<=)	Retorna verdadeiro caso o operando da esquerda seja menor ou igual ao da direita.	
