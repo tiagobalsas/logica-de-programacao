d414c834c6e6eee95ea2cc1e567703ee977a27eb
Tiago Santos <tiago881@gmail.com>
2021-09-09 10:24:38 -0300

    Exemplo: compara duas expressões e retorna false(falso)

--- operadores-logicos/operadores-logicos.js
+++ operadores-logicos/operadores-logicos.js
@@ -1,4 +1,5 @@
 // (E lógico) - AND lógico (&&)
 console.log(true && true); // retorna true
+console.log(true && false); // retorna false
 // (OU lógico) - OU lógico (||)
 // (Negação lógica) - NOT lógico (!)
