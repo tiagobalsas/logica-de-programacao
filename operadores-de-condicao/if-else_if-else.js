a0a2ed4b7a23d1059081229705ae9327c572f24e
Tiago Santos <tiago881@gmail.com>
2021-09-09 12:41:12 -0300

    Exemplos: operador IF

---
+++ operadores-de-condicao/if-else_if-else.js
@@ -0,0 +1,19 @@
+// if (condição) afirmação1
+
+// Não retorna a mensagem.
+const hora = 12;
+if (hora < 12) {
+  console.log('Bom dia!');
+}
+
+// Retorna Bom dia!
+const hora = 11;
+if (hora < 12) {
+  console.log('Bom dia!');
+}
+
+// Retorna Bom dia!
+const hora = 12;
+if (hora <= 12) {
+  console.log('Bom dia!');
+}
