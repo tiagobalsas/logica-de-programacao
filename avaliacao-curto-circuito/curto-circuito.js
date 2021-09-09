b89715ac613409d82a3f96e1ee3e5d575dbe935d
Tiago Santos <tiago881@gmail.com>
2021-09-09 11:14:03 -0300

    Avaliação curto circuíto

---
+++ avaliacao-curto-circuito/curto-circuito.js
@@ -0,0 +1,3 @@
+// (E lógico) - AND lógico (&&) - Todas expressões devem ser verdadeira para atender uma condição, caso contrário retorna falso e a checagem do código se encerra.
+
+// // (OU lógico) - OU lógico (||) - Se uma das expressões for verdadeira, a condição será atendida e a checagem do código continua. Só será falsa, quando todas as condições forem falsas. 
