e840a26705ac54f6375f2352a58dedf97c051f07
Tiago Santos <tiago881@gmail.com>
2021-09-09 10:32:35 -0300

    Exemplo: compara duas expressões e retorna true se um dos operadores for verdadeiro

--- operadores-logicos/operadores-logicos.js
+++ operadores-logicos/operadores-logicos.js
@@ -2,4 +2,7 @@
 console.log(true && true); // retorna true
 console.log(true && false); // retorna false
 // (OU lógico) - OU lógico (||)
+console.log(true || false); // retorna true
+console.log(false || true); // retorna true
+
 // (Negação lógica) - NOT lógico (!)
