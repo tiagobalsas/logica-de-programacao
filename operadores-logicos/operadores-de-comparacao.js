a7ece12166fc45173acf1145e39a6f150bea4fce
Tiago Santos <tiago881@gmail.com>
2021-09-09 09:53:40 -0300

    Exemplo:Compara igualdade,  tipo e  retorna false

--- operadores-logicos/operadores-de-comparacao.js
+++ operadores-logicos/operadores-de-comparacao.js
@@ -2,13 +2,16 @@
 let a = 1;
 let b = 1;
 console.log(a == b); // retorna true
-// Não igual (!=)	Retorna verdadeiro caso os operandos não sejam iguais.	
+// Não igual (!=)	Retorna verdadeiro caso os operandos não sejam iguais.
 let a = 1;
 let b = 1;
 console.log(a != b); // retorna false
-// Estritamente igual (===)	Retorna verdadeiro caso os operandos sejam iguais e do mesmo tipo. 
-// Estritamente não igual (!==)	Retorna verdadeiro caso os operandos não sejam iguais e/ou não sejam do mesmo tipo.	
-// Maior que (>)	Retorna verdadeiro caso o operando da esquerda seja maior que o da direita.	
-// Maior que ou igual (>=)	Retorna verdadeiro caso o operando da esquerda seja maior ou igual ao da direita.	
-// Menor que (<)	Retorna verdadeiro caso o operando da esquerda seja menor que o da direita.	
-// Menor que ou igual (<=)	Retorna verdadeiro caso o operando da esquerda seja menor ou igual ao da direita.	
+// Estritamente igual (===)	Retorna verdadeiro caso os operandos sejam iguais e do mesmo tipo.
+let a = 1;
+let b = '1';
+console.log(a === b); // retorna false
+// Estritamente não igual (!==)	Retorna verdadeiro caso os operandos não sejam iguais e/ou não sejam do mesmo tipo.
+// Maior que (>)	Retorna verdadeiro caso o operando da esquerda seja maior que o da direita.
+// Maior que ou igual (>=)	Retorna verdadeiro caso o operando da esquerda seja maior ou igual ao da direita.
+// Menor que (<)	Retorna verdadeiro caso o operando da esquerda seja menor que o da direita.
+// Menor que ou igual (<=)	Retorna verdadeiro caso o operando da esquerda seja menor ou igual ao da direita.
