676c6363fa3b372b6d1d9c141194bf9a00a4b08f
Tiago Santos <tiago881@gmail.com>
2021-09-09 14:49:14 -0300

    Exemplo: Operador lógico(&) dentro dentro do if

--- operadores-de-condicao/exemplos.js
+++ operadores-de-condicao/exemplos.js
@@ -1,5 +1,14 @@
 const numero = 10;
-
+//Se a condição for verdadeira, retorna o log.
 if (numero >= 0) {
   console.log('Sim, o número é maior que zero!');
+}
+
+// se a condição for verdareira, a validação para no primeiro log e imprime uma mensagem
+// Se não, executa a instrução dentro do else e imprime uma mensagem.
+const a = 5.1;
+if (a > 0 && a <= 5) {
+  console.log('O Valor de (a) está entre 1 e 5');
+} else {
+  console.log('O Valor de (a) é maior que 5');
 }
